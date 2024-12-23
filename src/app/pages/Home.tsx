aa80202b5c9a7296b1d8bf7e47b476b0eae0e0a6
vinicsperes <peresvini2@gmail.com>
2024-12-23 17:41:21 -0300

    idk: remove gridstack again!

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -4,8 +4,6 @@ import { Habit } from '@/utils/habitData'
 import { checkHabit, createHabit, fetchHabits } from '@/utils/fakeApi'
 import { useEffect, useState } from 'react'
 
-import { GridStack } from 'gridstack'
-import 'gridstack/dist/gridstack.min.css'
 import { HabitWidget } from '@/components/widgets/habit/HabitWidget'
 import '../../globals.css'
 
@@ -37,20 +35,6 @@ export default function Home() {
         else console.log(habitsResult.error)
     }, [])
 
-    useEffect(() => {
-        if (habits.length > 0) {
-            const grid = document.querySelector('.grid-stack') as HTMLElement
-            if (grid) {
-                GridStack.init({
-                    float: true,
-                    staticGrid: false,
-                    resizable: 'e, s, n, w' as any,
-                    cellHeight: 200,
-                    alwaysShowResizeHandle: false
-                }, grid)
-            }
-        }
-    }, [habits])
 
     return (
         <div className="container mx-2 p-4">
@@ -58,26 +42,19 @@ export default function Home() {
                 <h1 className="text-3xl font-bold mb-6">web-planner</h1>
                 <MultiStepDialog onAddHabit={handleAddHabit} />
             </div>
-            <div className="grid gap-6 md:grid-cols-2">
-                <div className="grid-stack bg-gray-700 rounded-lg">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Today's Habits</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-wrap gap-3">
                     {habits.map((habit) => (
-                        <div key={habit._id} className="grid-stack-item" data-gs-x="0" data-gs-y="0" data-gs-width="4" data-gs-height="2">
-                            <div className="grid-stack-item-content bg-gray-900">
-                                <HabitWidget key={habit._id} habit={habit} onUpdateHabitProgress={handleUpdateProgress} />
-                                {/* <HabitWidgetYear key={habit._id} habit={habit} onUpdateHabitProgress={onUpdateHabitProgress} /> */}
-                            </div>
+                        <div key={habit._id} className="flex justify-center w-min p-3 rounded-lg bg-gray-900">
+                            <HabitWidget key={habit._id} habit={habit} onUpdateHabitProgress={handleUpdateProgress} />
+                            {/* <HabitWidgetYear key={habit._id} habit={habit} onUpdateHabitProgress={onUpdateHabitProgress} /> */}
                         </div>
                     ))}
-                </div>
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Today's Habits</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        {/* <HabitList habits={habits} onUpdateHabitProgress={handleUpdateProgress} /> */}
-                    </CardContent>
-                </Card>
-            </div>
+                </CardContent>
+            </Card>
         </div>
     )
 }
