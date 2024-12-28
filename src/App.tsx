651f0908b45c23d724bda097f75fab84bba35315
vinicsperes <peresvini2@gmail.com>
2024-12-28 11:20:30 -0300

    feat: create a Toast to warn drag mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import RootLayout from './app/layout'
 import Home from './app/pages/Home'
+import { Toaster } from './components/ui/sonner'
 
 function App() {
     return (
         <RootLayout>
             <Home />
+            <Toaster position='bottom-right' richColors />
         </RootLayout>
     )
 }
--- src/components/floatingMenu/actions/RearrangeWidgetsButton.tsx
+++ src/components/floatingMenu/actions/RearrangeWidgetsButton.tsx
@@ -1,6 +1,7 @@
-import { Check, Move } from "lucide-react";
+import { Check, Hand, Move } from "lucide-react";
 import { Button } from "../../ui/button";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { toast } from "sonner";
 
 type ButtonProps = {
     setIsDragEnabled: (value: any) => void
@@ -8,40 +9,54 @@ type ButtonProps = {
 }
 
 export default function RearrangeWidgetsButton({ isDragEnabled, setIsDragEnabled }: ButtonProps) {
+    function handleMoveClick() {
+        setIsDragEnabled((prev: boolean) => {
+            const newValue = !prev;
+            toast(newValue ? "Drag enabled" : "Drag disabled", {
+                description: (
+                    <span>
+                        {newValue ?
+                            <span className="flex gap-1">Move items freely <Hand className="w-4 h-4" /></span>
+                            :
+                            <span className="flex gap-1">Editing completed <Check className="w-4 h-4" /></span>
+                        }
+                    </span>
+                ),
+            });
+            return newValue;
+        });
+    };
+
     return (
-        <Button 
+        <Button
             size="icon"
             className="h-9 w-9"
             variant="ghost"
-            onClick={() => setIsDragEnabled((prev: boolean) => !prev)}
+            onClick={handleMoveClick}
         >
             {isDragEnabled ?
-                <>
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <Check className="w-4 h-4" />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>Finish Editing</p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                </>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Check className="w-4 h-4" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Finish Editing</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
                 :
-                <>
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <Move className="w-4 h-4" />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>Rearrange Widgets</p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                </>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Move className="w-4 h-4" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Rearrange Widgets</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
             }
-        </Button>
+        </Button >
     )
 }
