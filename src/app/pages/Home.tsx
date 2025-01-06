550eb89a282b34b053416f769efa4721c204021b
vinicsperes <peresvini2@gmail.com>
2025-01-05 23:44:15 -0300

    fix: remove unsued react-grid-layout tips

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -4,15 +4,15 @@ import { Responsive, WidthProvider } from 'react-grid-layout';
 import { Habit, Todo, Widget } from '@/utils/habitData'
 import { checkHabit, createHabit, fetchHabits } from '@/utils/fakeApi'
 
+import TodoWidget from '@/components/widgets/todo/TodoWidget'
 import { HabitWidget } from "@/components/widgets/habit/HabitWidget"
 import { FloatingMenu } from "@/components/floatingMenu/FloatingMenu"
-import TodoWidget from '@/components/widgets/todo/TodoWidget'
-
-import '../../globals.css'
-import 'react-grid-layout/css/styles.css';
-import 'react-resizable/css/styles.css';
 import { DraggableWidget } from '@/components/widgets/components/draggableWidget/DraggableWidget';
 
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
+import '../../globals.css'
 const ResponsiveGridLayout = WidthProvider(Responsive)
 
 export default function Home() {
@@ -108,9 +108,6 @@ export default function Home() {
         })),
     }
 
-    const breakpoints = { lg: 1280, md: 992, sm: 768, xs: 480, xxs: 0 }
-    const cols = { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }
-
     return (
         <div className='flex flex-col h-screen px-48'>
             <FloatingMenu setIsDragEnabled={setIsDragEnabled} isDragEnabled={isDragEnabled} onAddHabit={handleAddHabit} />
@@ -121,12 +118,9 @@ export default function Home() {
                 <ResponsiveGridLayout
                     className="layout"
                     layouts={layouts}
-                    breakpoints={breakpoints}
-                    cols={cols}
                     rowHeight={150}
                     isResizable={false}
                     isDraggable={isDragEnabled}
-                    autoSize
                 >
                     {widgets.map((item) => (
                         <div key={item._id}>
