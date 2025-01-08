b78f80a611fd94867dd2657d334c0a4150a5e004
vinicsperes <peresvini2@gmail.com>
2025-01-07 21:19:28 -0300

    chore: remove framer-motion

--- src/components/widgets/todo/TodoWidget.tsx
+++ src/components/widgets/todo/TodoWidget.tsx
@@ -1,11 +1,12 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
-import { Plus, SquareCheckBig } from 'lucide-react';
-import { motion, AnimatePresence } from 'framer-motion';
-import { useTodoList } from './hooks/useTodoList';
 import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Plus, SquareCheckBig } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+import { useTodoList } from './hooks/useTodoList';
 import TodoItem from './TodoItem';
 
 export default function TodoWidget() {
@@ -30,46 +31,38 @@ export default function TodoWidget() {
             </CardHeader>
             <CardContent className='p-3'>
                 <ScrollArea className="pr-3">
-                    <motion.div layout className="space-y-3 max-h-[26rem]">
-                        <AnimatePresence>
-                            {tasks.map((task) => (
-                                <TodoItem
-                                    key={task.id}
-                                    task={task}
-                                    toggleTask={toggleTask}
-                                    addTask={addTask}
-                                    moveTask={moveTask}
-                                    removeTask={removeTask}
-                                    level={0}
-                                />
-                            ))}
-                            <motion.div
-                                initial={{ opacity: 0, y: -10 }}
-                                animate={{ opacity: 1, y: 0 }}
-                                exit={{ opacity: 0, y: -10 }}
-                                transition={{ duration: 0.2 }}
-                                className="flex items-center space-x-2 mt-2"
+                    <div className="space-y-3 max-h-[26rem]">
+                        {tasks.map((task) => (
+                            <TodoItem
+                                key={task.id}
+                                task={task}
+                                toggleTask={toggleTask}
+                                addTask={addTask}
+                                moveTask={moveTask}
+                                removeTask={removeTask}
+                                level={0}
+                            />
+                        ))}
+                        <div className="flex items-center space-x-2 mt-2">
+                            <input
+                                value={newTodo}
+                                onChange={(e) => setNewTodo(e.target.value)}
+                                placeholder="Add a new task..."
+                                onKeyPress={(e) => {
+                                    if (e.key === 'Enter' && newTodo.trim()) handleAddTodo();
+                                }}
+                                className="border-b border-muted-foreground bg-transparent focus:outline-none text-sm flex-grow"
+                            />
+                            <Button
+                                variant={'secondary'}
+                                size="icon"
+                                className="h-6 w-6"
+                                onClick={handleAddTodo}
                             >
-                                <input
-                                    value={newTodo}
-                                    onChange={(e) => setNewTodo(e.target.value)}
-                                    placeholder="Add a new task..."
-                                    onKeyPress={(e) => {
-                                        if (e.key === 'Enter' && newTodo.trim()) handleAddTodo();
-                                    }}
-                                    className="border-b border-muted-foreground bg-transparent focus:outline-none text-sm flex-grow"
-                                />
-                                <Button
-                                    variant={'secondary'}
-                                    size="icon"
-                                    className="h-6 w-6"
-                                    onClick={handleAddTodo}
-                                >
-                                    <Plus className='w-full h-full' />
-                                </Button>
-                            </motion.div>
-                        </AnimatePresence>
-                    </motion.div>
+                                <Plus className='w-full h-full' />
+                            </Button>
+                        </div>
+                    </div>
                 </ScrollArea>
             </CardContent>
         </Card>
